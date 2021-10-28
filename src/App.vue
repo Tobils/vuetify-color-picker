a8a39494db05baea6441f8bb90bb9526cb4fc31e
suhada <suhada@pts.sc>
2021-10-28 14:51:18 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <ColorPicker/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ColorPicker from './components/ColorPicker.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    ColorPicker
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main">
+    <div class="color-picker">
+      <v-color-picker
+        class="ma-2"
+        v-model="color"
+        :swatches="swatches"
+        show-swatches
+      ></v-color-picker>
+        <span>{{colors}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColorPicker",
+  data(){
+      return {
+          color: '',
+          colors: []
+      }
+  },
+  watch:{
+      color: function(){
+          console.log(this.color);
+          this.colors.push(this.color)
+      }
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  height: 100vh;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+</style>
